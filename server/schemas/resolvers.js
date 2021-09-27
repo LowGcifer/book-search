461fd6c719b75625de707d03aff2cc5bf8143f82
LowGcifer <LowGcifer@gmail.com>
2021-09-26 21:27:49 -0400

    initial commit

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async (parent, args) => {
+      return User.find({});
+    },
+    user: async (parent, args, context) => {
+      return User.findById(context.user._id).populate("savedBooks");
+    },
+    books: async (parent, args, context) => {
+      const user = User.findById(context.user._id).populate("savedBooks");
+      const { savedBooks } = user;
+      return savedBooks;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect email and password");
+      }
+
+      const correctPw = user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect email or password");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addBook: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user.id },
+          { $addToSet: { savedBooks: args } }
+        );
+      }
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
