9f6ef62305bdf82e0cfa19ff3f3c00da38212af6
LowGcifer <LowGcifer@gmail.com>
2021-09-26 21:56:22 -0400

    connected mongodb and heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+// For heroku connection
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
